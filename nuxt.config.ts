a841233df3a5a0a59810ecdaf50fc7f39237871c
Alessandro Pinheiro <alessandropsbra@gmail.com>
2024-03-28 15:24:14 -0300

    update images dir

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  modules: ["@nuxt/image", "nuxt-swiper", "@vesp/nuxt-fontawesome"],
 
   srcDir: "src/",
 
@@ -17,9 +18,9 @@ export default defineNuxtConfig({
   // },
 
   image: {
-    dir: 'src/public'
+    dir: './src/public/'
   },
 
-  modules: ["@nuxt/image", "nuxt-swiper", "@vesp/nuxt-fontawesome"],
+
 
 });
